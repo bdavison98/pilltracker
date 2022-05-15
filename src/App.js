3a250e9767c609c09b4e547b50460dbfa7096954
bdavison98 <bryanddavison@gmail.com>
2022-05-15 05:40:34 -0400

    Finised Add Medication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
 
 function TableHeader({labels}) {
-    const header = labels.map(name => <td>{name}</td>)
+    const header = labels.map(name => <td className=''>{name}</td>)
 
     return(
-        <thead> 
+        <thead className='text-xl font-bold h-20'> 
             {header}
         </thead>
     )
 }
 
 function TableRow({pillData}) {
-    const row = pillData.map((pill) => (
-        <tr>
+    const rows = pillData.map((pill) => (
+        <tr className='p-32'>
             <td>{pill.name}</td>
             <td>{pill.amount}</td>
             <td>{pill.dosage}mg</td>
@@ -24,34 +25,113 @@ function TableRow({pillData}) {
     
     return(
         <>
-            {row}
+            {rows}
         </>
     )
 }
 
-function MedTable(){
+function MedForm({medData,setMedData, setMedRows}) {
+
+    const [name, setName] = useState('')
+    const [amount, setAmount] = useState('')
+    const [dosage, setDosage] = useState('')
+    const [cost, setCost] = useState('')
+    const [formData, setFormData] = useState({})
+    const [submitted, setSubmitted] = useState(false)
     
+    if (submitted) {
+        setName('')
+        setAmount('')
+        setDosage('')
+        setCost('')
+        setSubmitted(false)
+        
+    }
+
+    return(
+        <div className='mt-20 inline-block p-8'>
+            <div className='block'>
+                <p className='underline text-left pb-2'>Name</p>
+                <input onChange={(e) => setName(e.target.value)} value={name} className='rounded-md p-2 bg-gray-800 block mb-8' type='text' placeholder='Name'></input>
+            </div>
+            <div className='block'>
+                <p className='underline text-left pb-2'>Amount</p>
+                <input onChange={(e) => setAmount(e.target.value)} value={amount} className='rounded-md p-2 bg-gray-800 block mb-8' type='text' placeholder='30'></input>
+            </div>
+            <div className='block'>
+                <p className='underline text-left pb-2'>Dosage</p>
+                <input onChange={(e) => setDosage(e.target.value)} value={dosage} className='rounded-md p-2 bg-gray-800 block mb-8' type='text' placeholder='500'></input>
+            </div>
+            <div className='block'>
+                <p className='underline text-left pb-2'>Cost</p>
+                <input onChange={(e) => setCost(e.target.value)} value={cost} className='rounded-md p-2 bg-gray-800 block mb-8' type='text' placeholder='5.99'></input>
+            </div>
+            <AddMedButton name={name} amount={amount} dosage={dosage} cost={cost} setMedData={setMedData} setMedRows={setMedRows} setSubmitted={setSubmitted}/>
+        </div>
+    )
+}
+
+function AddMedButton({ name, amount, dosage, cost, setMedData, setMedRows, setSubmitted}) {
+    function handleClick() {
+        setMedData({
+            name: name,
+            amount: amount,
+            dosage: dosage,
+            cost: cost
+        })
+        setMedRows(prevData => [...prevData, {
+            name: name,
+            amount: amount,
+            dosage: dosage,
+            cost: cost
+        }])
+        setSubmitted(true)
+    }
     return(
         <>
-            <TableHeader labels={["Name", "Amount", "Dosage", "Cost"]} />
-            <TableRow pillData={[{name: "Tylenol", amount:30, dosage:500, cost:5.99}]}/>
+            <button className='rounded-lg border-2 p-2' onClick={handleClick}>
+                Add Medication
+            </button>
         </>
+    )
+}
+function MedTable({medRows}){
+    
+    return(
+        <table className='w-1/2'>
+            <TableHeader labels={["Name", "Amount", "Dosage", "Cost"]} />
+            <TableRow className='' pillData={medRows}/>
+        </table>
     )
 }
 
 
 function MyMeds() {
+    const [medData, setMedData] = useState({})
+    const [medRows, setMedRows] = useState([])
+    console.log(medData)
+    console.log(medRows)
     return(
         <>
-            <MedTable/>
+            <h1 className='text-4xl'>My Medications</h1>
+            <div className='flex-wrap content-center'>
+                <MedTable medRows={medRows} medRows={medRows}/>
+            </div>
+            <div>
+                <MedForm setMedRows={setMedRows} setMedData={setMedData}/>
+                
+            </div>
+            
         </>
     )
 }
 
 function App() {
   return (
-    <div className="App">
-      <MyMeds></MyMeds>
+    <div className="App h-screen bg-gray-900">
+      <div className='text-white p-10'>
+          <MyMeds/>
+      </div>
     </div>
   );
 }
